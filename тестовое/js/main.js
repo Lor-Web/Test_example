707ff187eb2a3117c160c7e15e97a2a4391d5b34
Ruslan Ruya <68471014+Lor-Web@users.noreply.github.com>
2021-02-27 11:55:46 +0300

    Add files via upload

--- тестовое/js/main.js
+++ тестовое/js/main.js
@@ -3,8 +3,10 @@ const option = document.querySelectorAll("option");
 
 select.addEventListener("click", () => {
   if (select.getAttribute("size") === "5") {
+    select.style.backgroundPositionY = "center";
     select.removeAttribute("size");
   } else {
     select.setAttribute("size", 5);
+    select.style.backgroundPositionY = "13%";
   }
 });
